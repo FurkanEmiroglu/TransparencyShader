f0aa1efe457e957c3154910c98b464397155c9c4
Furkan Emiroglu <96686920+FurkanEmiroglu@users.noreply.github.com>
2022-10-24 22:35:43 +0300

    Gradual transparency shader

---
+++ WaveShader.shader
@@ -0,0 +1,112 @@
+﻿Shader "Unlit/WaveShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+		LOD 100
+		
+		ZTest GEqual
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert alpha
+			#pragma fragment frag alpha
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+			
+			uniform sampler2D _MainTex;
+			uniform float4 _MainTex_ST;
+
+			// light properties
+			uniform float3 _lightDirection;
+			uniform float4 _lightColor;
+
+			// dissolve starting wp
+			uniform float3 _dissolveStartingPos;
+
+			// vertex anim properties
+			uniform float _animationSpeed;
+			uniform float _deformAmount;
+
+			// transparency distance
+			uniform float _transparencyDistance;
+
+			struct meshData
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct Interpolators
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 normal : TEXCOORD1;
+				float3 vertexWP : TEXCOORD2;
+				float vertexDistance : TEXCOORD3;
+				UNITY_FOG_COORDS(1)
+			};
+
+			Interpolators vert (meshData v)
+			{
+				Interpolators o;
+				o.uv = v.uv;
+				o.vertex = v.vertex;
+				o.normal = UnityObjectToWorldNormal(v.normal);
+				o.vertexDistance = distance(mul(unity_ObjectToWorld, v.vertex).xyz, _dissolveStartingPos);
+
+				float3 deformDirection = o.normal.xyz;
+				o.vertex.xyz += abs(sin(_Time.y * _animationSpeed)) * deformDirection * _deformAmount;
+				
+				// calculating world position
+				o.vertexWP = mul(unity_ObjectToWorld, v.vertex).xyz;
+				
+				// clip positioning
+				o.vertex = UnityObjectToClipPos(o.vertex);
+				
+				// applying texture
+				o.uv = TRANSFORM_TEX(o.uv, _MainTex);
+				
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (Interpolators i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+
+				float light = dot(-_lightDirection, i.normal.xyz);
+				float4 returningColor;
+				
+				if (i.vertexDistance < _transparencyDistance)
+					returningColor = fixed4(0,0,0,0);
+				else if (i.vertexDistance < _transparencyDistance + 0.02)
+				{
+					fixed col = abs(sin(_Time.y * 250));
+					returningColor = fixed4(col.xxx,1);
+				}
+					
+				else
+				{
+					returningColor = col * (light * _lightColor);
+					returningColor.a = 1;
+				}
+
+				return returningColor;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ WaveShaderController.cs
@@ -0,0 +1,80 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Playground
+{
+    [SelectionBase]
+    public class WaveShaderController : MonoBehaviour
+    {
+        [Title("References")]
+        [Tooltip("We need this for applying light to our unlit shader")]
+        [SerializeField] private Light directionalLight;
+        [SerializeField] private Transform dissolveStartingPos;
+
+        [Title("Vertex Deform Coefficients")]
+        [Tooltip("Deforming loop speed")]
+        [SerializeField] private float animationSpeed;
+
+        [Tooltip("Vertex displacement amount")]
+        [SerializeField] private float deformAmount;
+
+        [SerializeField] [Range(0,2)] private float transparencyDistance;
+
+        #region Shader Property IDs
+
+        private static readonly int DissolveStartingPosID = Shader.PropertyToID("_dissolveStartingPos");
+        private static readonly int TransparencyDistanceID = Shader.PropertyToID("_transparencyDistance");
+        private static readonly int AnimationSpeedID = Shader.PropertyToID("_animationSpeed");
+        private static readonly int DeformAmountID = Shader.PropertyToID("_deformAmount");
+        private static readonly int LightColorID = Shader.PropertyToID("_lightColor");
+        private static readonly int LightDirectionID = Shader.PropertyToID("_lightDirection");
+
+        #endregion
+        
+        private Renderer rend;
+
+        private void Awake()
+        {
+            rend = GetComponentInChildren<Renderer>();
+        }
+
+        private void Start()
+        {
+            SetLightPos();
+            SetDissolveStartingPos();
+        }
+
+        private void Update()
+        {
+            SetLightColor();
+            SetDeformCoeffs();
+            SetTransparencyDistance();
+        }
+
+        private void SetDissolveStartingPos()
+        {
+            rend.material.SetVector(DissolveStartingPosID, dissolveStartingPos.position);
+        }
+
+        private void SetTransparencyDistance()
+        {
+            rend.material.SetFloat(TransparencyDistanceID, transparencyDistance);
+        }
+
+        private void SetDeformCoeffs()
+        {
+            rend.material.SetFloat(AnimationSpeedID, animationSpeed);
+            rend.material.SetFloat(DeformAmountID, deformAmount);
+        }
+
+        private void SetLightColor()
+        {
+            rend.material.SetColor(LightColorID,directionalLight.color);
+        }
+
+        private void SetLightPos()
+        {
+            rend.material.SetVector(LightDirectionID, directionalLight.transform.forward);
+        }
+    }
+}
